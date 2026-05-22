5ee38402b0f78fbc67a231f40c20124848f48085
shariful-ire <shariful.ire@gmail.com>
2026-05-22 16:42:28 +0600

    install the react icon

--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -1,27 +1,30 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import { FaUser } from "react-icons/fa";
 
 const Players = ({ playerPromise }) => {
+  const playerData = use(playerPromise);
 
-    const playerData = use(playerPromise);
+  return (
+    <div>
+      <h1>Total players : {playerData.length}</h1>
 
-    return (
-        <div>
-
-            <h1>Total players : {playerData.length}</h1>
-
-            {
-                playerData.map(player => (
-                    <div key={player.id}>
-                        <img src={player.playerImage}></img>
-                        <h1>Name: {player.playerName}</h1>
-                        
-                        
-                    </div>
-                ))
-            }
-
+      {playerData.map((player) => (
+        <div key={player.id}>
+          <div className="$$card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img
+                src={player.playerImage}
+                alt="Player Image"
+              />
+            </figure>
+            <h1><FaUser /> {player.playerName}</h1>
+           
+          </div>
+          
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Players;
